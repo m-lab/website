c7279be757da3317dfcd32d299e4fbd2d51de821
Andrew Newhouse <andrewwn@gmail.com>
2016-04-05 13:44:10 -0400

    adds 3 js event listeners for mobile nav menu functionality

--- js/src/controllers/enhancers/NavigationEnhancer.js
+++ js/src/controllers/enhancers/NavigationEnhancer.js
@@ -42,6 +42,42 @@ function(
         }
       });
 
+      //minor event listener on mobile menu accordions to flip the arrow
+      // glyphicon depending if the particular accordion is open or closed.
+      $('.mobile-sub-nav').on('show.bs.collapse', function(){
+        $(this).parent().find(".glyphicon-triangle-bottom") 
+          .removeClass("glyphicon-triangle-bottom") 
+          .addClass("glyphicon-triangle-top");
+      }).on('hide.bs.collapse', function(){
+        $(this).parent().find(".glyphicon-triangle-top")
+          .removeClass("glyphicon-triangle-top") 
+          .addClass("glyphicon-triangle-bottom");
+      });
+
+      //click event logic that decides if a top level nav item follow the href
+      //link or act as an accordion since on desktop, it is just a link, but on
+      // mobile, it is an accordion.
+      $('.mobile-sub-nav-trigger').click( function(e) {
+          if ( $('#mobile-main-nav').is(":visible") ) {
+            e.preventDefault();
+            $(this).attr("href", "#"+$(this).attr("data-target"));
+
+            var parent = $(this).data('parent');
+            var actives = parent && $(parent).find('.collapse.in');
+
+            if (actives && actives.length) {
+                hasData = actives.data('collapse');
+                actives.collapse('hide');
+            }
+
+            $('#'+$(this).attr("data-target")).collapse('toggle');
+
+          } else {
+            $(this).attr("href", $(this).attr("data-link"));
+          }
+        }
+      );
+
     }
   };
 
