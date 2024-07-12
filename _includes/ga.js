a4f3114dbcfa9b0df83cffa201e604e5877ed4aa
nkinkade <kinkade@measurementlab.net>
2024-07-12 11:29:54 -0400

    Fixes Analytics GA4 tag inclusion (#805)
    The way I had done it previously was resulting in nested <script> tags, which
    was, I think, breakting things. This commit splits out the loading of gtag/js
    into its own script tag, and reduces ga.js to only javascript.

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,10 +1,5 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GPGKR8WBC6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-GPGKR8WBC6');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-GPGKR8WBC6');
 
