ebf7d093c6fb43d0a8b5bd572a194aefc16ebd52
Nathan Kinkade <kinkade@measurementlab.net>
2025-06-10 16:31:49 -0600

    Configures status map to use Locate registration information
    The old siteinfo geo format does not include host-managed servers. The
    information from Locate is not as rich, but is probably good enough for the
    map. Additionally Locate's database doesn't know about "sites", but instead
    knows about heartbeat messages from individual experiments, which has the
    effect of breaking out M-Lab traditional site markes into mlab1, mlab2, mlab3.
    This may not be a bad thing, since it gives a better picture of which machines
    are out there, not just which sites.

--- _includes/infrastructure-map.js
+++ _includes/infrastructure-map.js
@@ -2,16 +2,17 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibS1sYWIiLCJhIjoiY2xsdmN2NXBuMTl1ZDNmcGh1Zmxma
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/dark-v10',
-  center: [5,17],
+  center: [5, 17],
   zoom: 1.2
 });
 
-var url = 'https://siteinfo.mlab-oti.measurementlab.net/v1/sites/geo.json';
+//var url = 'https://siteinfo.mlab-oti.measurementlab.net/v1/sites/geo.json';
+var url = 'https://locate-dot-mlab-sandbox.appspot.com/v2/siteinfo/registrations?format=geo&exp=ndt';
 
 map.on('load', function () {
 
-  const tenG = ['==', ['get','uplink'], '10g'];
-  const oneG = ['==', ['get','uplink'], '1g'];
+  const tenG = ['==', ['get', 'uplink'], '10g'];
+  const oneG = ['==', ['get', 'uplink'], '1g'];
 
   map.addSource("mlab-sites", {
     type: "geojson",
@@ -38,11 +39,12 @@ map.on('load', function () {
       ],
       "circle-color": [
         "step",
-          ["get","point_count"],
-          "#deebf7",
-          3, "#9ecae1",
-          6, "#3182bd"
-      ]}
+        ["get", "point_count"],
+        "#deebf7",
+        3, "#9ecae1",
+        6, "#3182bd"
+      ]
+    }
   });
   map.addLayer({
     "id": "cluster-count",
@@ -53,7 +55,7 @@ map.on('load', function () {
       "text-field": "{point_count_abbreviated}",
       "text-font": ["DIN Offc Pro Medium",
         "Arial Unicode MS Bold"],
-        "text-size": 12
+      "text-size": 12
     }
   });
   map.addLayer({
@@ -76,7 +78,7 @@ map.on('load', function () {
       "text-field": "1",
       "text-font": ["DIN Offc Pro Medium",
         "Arial Unicode MS Bold"],
-        "text-size": 12
+      "text-size": 12
     }
   });
 
@@ -87,21 +89,18 @@ map.on('load', function () {
     className: 'point-popup'
   });
 
-  map.on('click','clusters', function(e) {
+  map.on('click', 'clusters', function (e) {
     var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
     var clusterId = features[0].properties.cluster_id,
-    point_count = features[0].properties.point_count,
-    clusterSource = map.getSource('mlab-sites');
-    clusterNodes = clusterSource.getClusterLeaves(clusterId, point_count, 0, function(err, aFeatures){
+      point_count = features[0].properties.point_count,
+      clusterSource = map.getSource('mlab-sites');
+    clusterNodes = clusterSource.getClusterLeaves(clusterId, point_count, 0, function (err, aFeatures) {
       var desc = "";
-      for (c=0; c < aFeatures.length; c++) {
-        desc += "<div class='pod-popup'><h4>"+aFeatures[c].properties.city+" - "+
-          aFeatures[c].properties.name+" - "+aFeatures[c].properties.uplink+"</h4>"+
-          aFeatures[c].properties.provider + " ("+aFeatures[c].properties.asn+")<br>"+
-          "IPv4 Prefix: "+aFeatures[c].properties.ipv4_prefix;
-        if (aFeatures[c].properties.ipv6_prefix != null) {
-          desc += "<br>IPv6 Prefix: "+aFeatures[c].properties.ipv6_prefix;
-        }
+      for (c = 0; c < aFeatures.length; c++) {
+        desc += "<div class='pod-popup'><h4>" + aFeatures[c].properties.hostname + "</h4>" +
+          "Provider: " + aFeatures[c].properties.org + "<br/>" +
+          "Uplink: " + aFeatures[c].properties.uplink + "<br/>" +
+          "Type: " + aFeatures[c].properties.type;
         desc += "</div>";
       }
       var coordinates = e.lngLat;
@@ -112,15 +111,12 @@ map.on('load', function () {
     });
   });
 
-  map.on('click','unclustered-point', function(e) {
+  map.on('click', 'unclustered-point', function (e) {
     var coordinates = e.features[0].geometry.coordinates.slice();
-    var description = "<h4>" + e.features[0].properties.city + " - " +
-      e.features[0].properties.name + " - "+ e.features[0].properties.uplink +"</h4>" +
-      e.features[0].properties.provider + " ("+e.features[0].properties.asn + ")<br>" +
-      "IPv4 Prefix: " + e.features[0].properties.ipv4_prefix;
-      if (e.features[0].properties.ipv6_prefix != null ) {
-        description += "<br>IPv6 Prefix: " + e.features[0].properties.ipv6_prefix;
-      }
+    var description = "<h4>" + aFeatures[c].properties.hostname + "</h4>" +
+      "Provider: " + aFeatures[c].properties.org + "<br/>" +
+      "Uplink: " + aFeatures[c].properties.uplink + "<br/>" +
+      "Type: " + aFeatures[c].properties.type;
 
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
@@ -128,16 +124,16 @@ map.on('load', function () {
     pointPopup.setLngLat(coordinates).setHTML(description).addTo(map);
   });
 
-  map.on('mouseenter','clusters', function (e) {
+  map.on('mouseenter', 'clusters', function (e) {
     map.getCanvas().style.cursor = 'pointer';
   });
-  map.on('mouseleave','clusters', function () {
+  map.on('mouseleave', 'clusters', function () {
     map.getCanvas().style.cursor = '';
   });
-  map.on('mouseenter','unclustered-point', function (e) {
+  map.on('mouseenter', 'unclustered-point', function (e) {
     map.getCanvas().style.cursor = 'pointer';
   });
-  map.on('mouseleave','unclustered-point', function () {
+  map.on('mouseleave', 'unclustered-point', function () {
     map.getCanvas().style.cursor = '';
   });
 });
